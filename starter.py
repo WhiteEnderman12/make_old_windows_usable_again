3a7927cffbc896c90e502dc4c570ff69da99e198
WhiteEnderman12 <157729585+WhiteEnderman12@users.noreply.github.com>
2025-10-07 17:43:13 +0000

    Update starter.py
    Starts Backend Simulator now too

--- starter.py
+++ starter.py
@@ -8,3 +8,12 @@ except Exception as e:
 	file = open("errorlog.log", "a")
 	file.write(str(e))
 	file.close()
+
+try:
+	print("starting Backend Simulator...")
+	process = subprocess.Popen(["python", "backendsimulator/backendsimulator.py"])
+except Exception as e:
+	print("start failed! See errorlog")
+	file = open("errorlog.log", "a")
+	file.write(str(e))
+	file.close()
